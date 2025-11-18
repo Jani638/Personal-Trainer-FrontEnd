8d3112b263386e03e961fab56d47acebe489a240
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2025-11-18 10:18:53 +0200

    Customers datagrid

--- src/Components/CustomerList.tsx
+++ src/Components/CustomerList.tsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react";
 import type { Tcustomer } from "../types";
+import type { GridColDef} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 
 
 
 export default function CustomerList(){
     const [customers, setCustomers] = useState<Tcustomer[]>([]);
+
+    const columns: GridColDef[] = [
+        {field: 'firstname', headerName: 'First Name', flex: 1},
+        {field: 'lastname', headerName: 'Last Name', flex: 1},
+        {field: 'streetaddress', headerName: 'Street Address', flex: 1},
+        {field: 'postcode', headerName: 'Postalcode', flex: 1},
+        {field: 'city', headerName: 'City', flex: 1},
+        {field: 'email', headerName: 'Email', flex: 1},
+        {field: 'phone', headerName: 'Phone', flex: 1}
+    ]
 
     const getCustomers = async () => {
         try {
@@ -22,7 +34,11 @@ export default function CustomerList(){
 
     return (
         <div>
-            <h2>Customers</h2>
+            <DataGrid 
+            getRowId={(row) => row._links.self.href} 
+            rows={customers} 
+            columns={columns} 
+            />
         </div>
     );
 }
--- src/types.tsx
+++ src/types.tsx
@@ -1,4 +1,5 @@
 export type Tcustomer = {
+    id: number;
     firstname: string;
     lastname: string;
     email: string;

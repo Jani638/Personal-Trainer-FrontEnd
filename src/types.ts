8857220868c97b767b58aff28791bf73fb35605e
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2025-11-28 09:09:09 +0200

    Virheen korjauksia buildissa

---
+++ src/types.ts
@@ -0,0 +1,55 @@
+export type Tcustomer = {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    phone: string;
+    streetaddress: string;
+    postcode: string;
+    city: string;
+}
+
+export type Ttrainings = {
+    date: string;
+    duration: number;
+    activity: string;
+}
+
+export type AddCustomerProps = {
+    handleAdd: (customer: Tcustomer) => void;
+    setSnackbarOpen: (open: boolean) => void;
+    setSnackbarMessage: (message: string) => void;
+}
+
+export type EditCustomerProps = {
+    url: string;
+    currentCustomer: Tcustomer;
+    handleUpdate: (url: string, customer: Tcustomer) => void;
+    setSnackbarOpen: (open: boolean) => void;
+    setSnackbarMessage: (message: string) => void;
+}
+
+export type AddTrainingProps = {
+    customerLink: string;
+    firstname: string;
+    lastname: string;
+    handleAdd: (training: Ttrainings & { customer: string }) => void;
+    setSnackbarOpen: (open: boolean) => void;
+    setSnackbarMessage: (message: string) => void;
+}
+
+export type CustomToolbarProps = {
+    title: string;
+    handleAdd?: (customer: Tcustomer) => void;
+    setSnackbarOpen?: (open: boolean) => void;
+    setSnackbarMessage?: (message: string) => void;
+    exportFields?: string[];
+    showAddButton?: boolean;
+}
+
+export type CalendarEvent = {
+    title: string;
+    start: string;
+    end: string;
+    backgroundColor?: string;
+}
